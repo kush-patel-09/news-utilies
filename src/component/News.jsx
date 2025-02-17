b7bebb18eb2463f978d3a0c57a3d667a1161f69b
kush patel <kushpat09@gmail.com>
2025-02-17 16:46:37 +0530

    working with fatch api

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -7,19 +7,46 @@ export default class News extends Component {
     constructor(){
         super();
         this.state={ 
-            allArtical:[]
+            allArtical:[],
+            pageNo:1
         }
     }
     async componentDidMount(){
-        let url= 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1c89ee141c4e4c17a238a7218c972b9e';
+        let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1c89ee141c4e4c17a238a7218c972b9e&page=${ this.state.pageNo}`;
         let data= await fetch (url);
         let pareseData= await data.json();
         // console.log(pareseData);
 
         this.setState({
             allArtical:pareseData.articles
+        });
+        
+        
+    }
+
+    handleNextClick = async () => {
+        let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1c89ee141c4e4c17a238a7218c972b9e&page=${ this.state.pageNo+1}`;
+        let data= await fetch (url);
+        let pareseData= await data.json();
+        // console.log(pareseData);
+
+        this.setState({
+            allArtical:pareseData.articles,
+            pageNo: this.state.pageNo+1
         })
     }
+    handleprevClick = async () => {
+        let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1c89ee141c4e4c17a238a7218c972b9e&page=${ this.state.pageNo-1}`;
+        let data= await fetch (url);
+        let pareseData= await data.json();
+        // console.log(pareseData);
+
+        this.setState({
+            allArtical:pareseData.articles,
+            pageNo: this.state.pageNo-1
+        })
+    }
+    
     render() {
         return (
             <>
@@ -35,7 +62,12 @@ export default class News extends Component {
                             url={single.url}/>  
                         })}
 
-                        
+                        <div className="row">
+                            <div className="col">
+                                <button className="btn btn-primary float-start" onClick={this.handleprevClick}>Previous</button>
+                                <button className="btn btn-primary float-end"onClick={this.handleNextClick}>Next</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </>
